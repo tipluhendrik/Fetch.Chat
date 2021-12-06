31c1d854e852ead0521096a34d7035f1ff41c29c
tipluhendrik <h.intveen@tiplu.de>
2021-12-06 10:53:25 +0100

    ChatHub hinzugefügt

---
+++ Fetch.Chat.Web/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Fetch.Chat.Domain;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Fetch.Chat.Web
+{
+    public class ChatHub : Hub
+    {
+        private readonly IChatService _chatService;
+
+        public ChatHub(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            _chatService.AddMessage(new(user, message));
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task<IReadOnlyCollection<Message>> GetAllMessages()
+        {
+            return Task.FromResult(_chatService.GetAllMessages());
+        }
+    }
+}
--- Fetch.Chat.Web/Program.cs
+++ Fetch.Chat.Web/Program.cs
@@ -1,5 +1,6 @@
 using Fetch.Chat.Domain;
 using Fetch.Chat.Infrastructure;
+using Fetch.Chat.Web;
 using Fetchchat;
 using Grpc.Core;
 
@@ -11,12 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 IChatService chatService = new ChatService();
 builder.Services.AddSingleton<IChatService>(chatService);
 
 var app = builder.Build();
 
+app.MapHub<ChatHub>("/chatHub");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
