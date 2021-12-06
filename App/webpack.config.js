b8faef0f2bea6a3aa1627da4596e5d1f19607871
TipluSen <s.chung@tiplu.de>
2021-12-06 10:35:57 +0100

    frontend app added

---
+++ App/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.min.js",
+  },
+};
